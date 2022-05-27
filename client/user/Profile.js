4ca086cc3954762db5a2dc3e14951822c7312c8a
Alan Yap <alan.yap@dpc.vic.gov.au>
2022-05-27 15:34:17 +1000

    Custom views for components incl listing all users, signup, viewing profile, editing profile, deleting user and methods to access each of the backend API endpoints

---
+++ client/user/Profile.js
@@ -0,0 +1,106 @@
+// Note you cannot use conflicting versions of material ui core and material ui icons
+// This could be the reason for errors such as (0 , _createSvgIcon.default) is not a function
+
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Avatar from "@material-ui/core/Avatar";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+import Edit from "@material-ui/icons/Edit";
+import Person from "@material-ui/icons/Person";
+
+import { Link, Redirect } from "react-router-dom";
+
+import auth from "./../auth/authHelper";
+import { read } from "./apiUser";
+import DeleteUser from "./DeleteUser";
+
+const useStyles = makeStyles((theme) => ({
+  root: theme.mixins.gutters({
+    maxWidth: 600,
+    margin: "auto",
+    padding: theme.spacing(3),
+    marginTop: theme.spacing(5),
+  }),
+  title: {
+    marginTop: theme.spacing(3),
+    color: theme.palette.protectedTitle,
+  },
+}));
+
+export default function Profile({ match }) {
+  const classes = useStyles();
+  const [user, setUser] = useState([]);
+  const [redirectToSignin, setRedirectToSignin] = useState(false);
+  const jwt = auth.isAuthenticated();
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    read(
+      {
+        userId: match.params.userId,
+      },
+      { t: jwt.token },
+      signal
+    ).then((data) => {
+      if (data && data.error) {
+        setRedirectToSignin(true);
+      } else {
+        setUser(data);
+      }
+    });
+    return function cleanup() {
+      abortController.abort();
+    };
+  }, [match.params.userId]);
+
+  if (redirectToSignin) {
+    return <Redirect to="/signin" />;
+  }
+
+  return (
+    <Paper className={classes.root} elevation={4}>
+      <Typography variant="h6" className={classes.title}>
+        Profile
+      </Typography>
+      <List dense>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <Person />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={user.name} secondary={user.email} />
+          {auth.isAuthenticated().user &&
+            auth.isAuthenticated().user._id == user._id && (
+              <ListItemSecondaryAction>
+                <Link to={"/user/edit/" + user._id}>
+                  <IconButton aria-label="Edit" color="primary">
+                    <Edit />
+                  </IconButton>
+                </Link>
+                <DeleteUser userId={user._id} />
+              </ListItemSecondaryAction>
+            )}
+        </ListItem>
+        <Divider />
+        <ListItem>
+          <ListItemText
+            primary={"Joined: " + new Date(user.created).toDateString()}
+          />
+        </ListItem>
+      </List>
+    </Paper>
+  );
+}
---
+++ client/user/Signup.js
@@ -0,0 +1,143 @@
+// Note you cannot use conflicting versions of material ui core and material ui icons
+// This could be the reason for errors such as (0 , _createSvgIcon.default) is not a function
+
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { Link } from "react-router-dom";
+import Icon from "@material-ui/core/Icon";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import { create } from "./apiUser";
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    maxWidth: 600,
+    margin: "auto",
+    textAlign: "center",
+    marginTop: theme.spacing(5),
+    paddingBottom: theme.spacing(2),
+  },
+  error: {
+      verticalAlign: "middle"
+  },
+  submit: {
+      margin: "auto",
+      marginBottom: theme.spacing(2)
+  },
+  textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 300
+  },
+  title: {
+    marginTop: theme.spacing(2),
+    color: theme.palette.openTitle,
+  },
+}));
+
+export default function Signup() {
+  const classes = useStyles();
+  const [values, setValues] = useState({
+    name: "",
+    password: "",
+    email: "",
+    open: false,
+    error: "",
+  });
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const clickSubmit = () => {
+    const user = {
+      name: values.name || undefined,
+      email: values.email || undefined,
+      password: values.password || undefined,
+    };
+
+    create(user).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setValues({ ...values, error: "", open: true });
+      }
+    });
+  };
+    
+  return (
+    <div>
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography variant="h6" className={classes.title}>
+            Sign Up
+          </Typography>
+          <TextField
+            id="name"
+            label="Name"
+            className={classes.textField}
+            value={values.name}
+            onChange={handleChange("name")}
+            margin="normal"
+          />
+          <br />
+          <TextField
+            id="email"
+            type="email"
+            label="Email"
+            className={classes.textField}
+            value={values.email}
+            onChange={handleChange("email")}
+            margin="normal"
+          />
+          <br />
+          <TextField
+            id="password"
+            type="password"
+            label="Password"
+            className={classes.textField}
+            value={values.password}
+            onChange={handleChange("password")}
+            margin="normal"
+          />
+          <br />
+          {values.error && (
+            <Typography component="p" color="error">
+              <Icon color="error" className={classes.error}>
+                error
+              </Icon>
+              {values.error}
+            </Typography>
+          )}
+        </CardContent>
+        <CardActions>
+            <Button color="primary" variant="contained" onClick={clickSubmit}>Submit</Button>
+        </CardActions>
+      </Card>
+      <Dialog open={values.open} disableBackdropClick={true}>
+          <DialogTitle>New Account</DialogTitle>
+          <DialogContent>
+              <DialogContentText>
+                  New account successfully created.
+              </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+              <Link to="/signin">
+                  <Button color="primary" autoFocus="autoFocus" variant="contained">
+                      Sign In
+                  </Button>
+              </Link>
+          </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
